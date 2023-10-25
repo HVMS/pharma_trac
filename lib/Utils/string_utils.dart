974839306dce9ce597372710a51638808583d73e
HVMS <viralsid2330@gmail.com>
2023-10-24 22:04:30 -0300

    string utils file

---
+++ lib/Utils/string_utils.dart
@@ -0,0 +1,12 @@
+class StringUtils{
+
+  static const String analytics = 'Analytics';
+  static const String adr = 'ADR';
+  static const String chat = 'Chat';
+  static const String vitals = 'Vitals';
+  static const String profile = 'Profile';
+
+  // Splash Screen Caption
+  static const String splashScreenCaption = 'Safe guarding Against Adverse Drug Reactions';
+
+}
