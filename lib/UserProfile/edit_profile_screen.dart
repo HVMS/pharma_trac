ed9c830226ba24c8299bcc56e4519ac9d6a49878
HVMS <viralsid2330@gmail.com>
2023-10-25 17:21:57 -0300

    Edit profile screen created

---
+++ lib/UserProfile/edit_profile_screen.dart
@@ -0,0 +1,224 @@
+import 'package:flutter/material.dart';
+import 'package:pharma_trac/Utils/string_utils.dart';
+import 'package:google_fonts/google_fonts.dart';
+import '../Utils/colors_utils.dart';
+
+class EditProfileScreen extends StatelessWidget{
+
+  const EditProfileScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          StringUtils.editScreenAppBarTitle,
+          style: GoogleFonts.roboto(
+            fontSize: 18.0,
+            fontWeight: FontWeight.w700,
+            color: ColorUtils.white,
+          ),
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          StringUtils.name,
+                          style: GoogleFonts.kiwiMaru(
+                            fontSize: 15.0,
+                            fontWeight: FontWeight.w700,
+                            color: ColorUtils.black,
+                          ),
+                        ),
+                        Text(
+                          StringUtils.userFullName,
+                          style: GoogleFonts.kiwiMaru(
+                            fontSize: 15.0,
+                            color: ColorUtils.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const CircleAvatar(
+                    backgroundImage: AssetImage('images/arrow_forward.png'),
+                    backgroundColor: Colors.transparent,
+                    radius: 15,
+                  ),
+                ],
+              ),
+              const SizedBox(height: 5),
+              Divider(
+                height: 20.0,
+                thickness: 2.0,
+                color: ColorUtils.grey,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          StringUtils.emailAddress,
+                          style: GoogleFonts.kiwiMaru(
+                            fontSize: 15.0,
+                            fontWeight: FontWeight.w700,
+                            color: ColorUtils.black,
+                          ),
+                        ),
+                        Text(
+                          StringUtils.userFullName,
+                          style: GoogleFonts.kiwiMaru(
+                            fontSize: 15.0,
+                            color: ColorUtils.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const CircleAvatar(
+                    backgroundImage: AssetImage('images/arrow_forward.png'),
+                    backgroundColor: Colors.transparent,
+                    radius: 15,
+                  ),
+                ],
+              ),
+              const SizedBox(height: 5),
+              Divider(
+                height: 20.0,
+                thickness: 2.0,
+                color: ColorUtils.grey,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          StringUtils.countryName,
+                          style: GoogleFonts.kiwiMaru(
+                            fontSize: 15.0,
+                            fontWeight: FontWeight.w700,
+                            color: ColorUtils.black,
+                          ),
+                        ),
+                        Text(
+                          StringUtils.userFullName,
+                          style: GoogleFonts.kiwiMaru(
+                            fontSize: 15.0,
+                            color: ColorUtils.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const CircleAvatar(
+                    backgroundImage: AssetImage('images/arrow_forward.png'),
+                    backgroundColor: Colors.transparent,
+                    radius: 15,
+                  ),
+                ],
+              ),
+              const SizedBox(height: 5),
+              Divider(
+                height: 20.0,
+                thickness: 2.0,
+                color: ColorUtils.grey,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          StringUtils.height,
+                          style: GoogleFonts.kiwiMaru(
+                            fontSize: 15.0,
+                            fontWeight: FontWeight.w700,
+                            color: ColorUtils.black,
+                          ),
+                        ),
+                        Text(
+                          StringUtils.userFullName,
+                          style: GoogleFonts.kiwiMaru(
+                            fontSize: 15.0,
+                            color: ColorUtils.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const CircleAvatar(
+                    backgroundImage: AssetImage('images/arrow_forward.png'),
+                    backgroundColor: Colors.transparent,
+                    radius: 15,
+                  ),
+                ],
+              ),
+              const SizedBox(height: 5),
+              Divider(
+                height: 20.0,
+                thickness: 2.0,
+                color: ColorUtils.grey,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                          StringUtils.weight,
+                          style: GoogleFonts.kiwiMaru(
+                            fontSize: 15.0,
+                            fontWeight: FontWeight.w700,
+                            color: ColorUtils.black,
+                          ),
+                        ),
+                        Text(
+                          StringUtils.userFullName,
+                          style: GoogleFonts.kiwiMaru(
+                            fontSize: 15.0,
+                            color: ColorUtils.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const CircleAvatar(
+                    backgroundImage: AssetImage('images/arrow_forward.png'),
+                    backgroundColor: Colors.transparent,
+                    radius: 15,
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
