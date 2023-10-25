b72c80bdbcd4c7fc7bde9838b102ef4dbe92d101
HVMS <viralsid2330@gmail.com>
2023-10-24 22:04:09 -0300

    color utils file

---
+++ lib/Utils/colors_utils.dart
@@ -0,0 +1,17 @@
+import 'dart:ui';
+import 'package:hexcolor/hexcolor.dart';
+
+class ColorUtils{
+
+  static final Color primaryColor = HexColor("#579BDA");
+  static final Color secondaryColor = HexColor("#5643CD");
+  static final Color appBackgroundColor = HexColor("#DFF0FF");
+  
+  // Bottom sheet Navigation bar colors
+  static final Color bottomNavigationItemColor = HexColor("#62B3FF");
+  static final Color navBackgroundColor = HexColor("#FFFFFF");
+
+  // Splash screen text color
+  static final Color splashScreenTextColor = HexColor("#FAF000");
+
+}
