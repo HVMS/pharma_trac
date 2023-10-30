a607cd30331ac700c8612488bbf9132b7c0cd64e
HVMS <viralsid2330@gmail.com>
2023-10-30 16:51:10 -0300

    user model initiated

---
+++ lib/model/user.model.dart
@@ -0,0 +1,17 @@
+import 'dart:core';
+
+class Users{
+  late final String? emailAddress;
+  // late final String? fullName;
+  late final String? password;
+  // late final DateTime? birthDate;
+  // late final String? gender;
+  // late final String? country;
+  // late final double? height;
+  // late final double? weight;
+
+  // Users({this.emailAddress, this.fullName, this.password, this.birthDate, this.gender, this.country, this.height, this.weight});
+
+  Users({this.emailAddress, this.password});
+
+}
