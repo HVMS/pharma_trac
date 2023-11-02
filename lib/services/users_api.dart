26b09bb61d462f58339f3eda25a284998160c8ee
HVMS <viralsid2330@gmail.com>
2023-11-01 21:49:51 -0300

    users api updated for registration part only

--- lib/services/users_api.dart
+++ lib/services/users_api.dart
@@ -1,21 +1,59 @@
+import 'dart:convert';
+
 import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
+import 'package:pharma_trac/model/user.model.dart';
+
+import '../model/user_register_response_model.dart';
 
 class UsersAPI{
-  static const baseURL = "";
+  static const baseURL = "https://pharma-trac-backend-host.onrender.com/";
 
-  static addUser(Map userData) async{
-    try {
-      final res = await http.post(Uri.parse("uri"), body: userData);
+  static var client = http.Client();
 
-      if (res.statusCode == 200){
-        //
-      } else{
-        //
-      }
-    } on Exception catch (e) {
-      debugPrint(e.toString());
+  static Future<String> register(UserRegisterRequestModel model) async {
+    Map<String, String> requestHeaders = {
+      'content-type' : 'application/json',
+    };
+
+    var urlFinal = Uri.parse("${baseURL}register");
+
+    var response = await client.post(
+      urlFinal,
+      headers: requestHeaders,
+      body: jsonEncode(model.toJson()),
+    );
+
+    if(response.statusCode == 200){
+      //
+      return response.body;
+    }else{
+      //
+      return response.body;
     }
-
   }
+
+  // static addUser(Map userData) async{
+  //
+  //   print(userData);
+  //
+  //   var addUserURL = Uri.parse("${baseURL}register");
+  //
+  //   print("Hello wrold");
+  //
+  //   try {
+  //     final res = await http.post(addUserURL, body: userData);
+  //
+  //     if (res.statusCode == 200){
+  //       // data
+  //       var data = jsonDecode(res.body.toString());
+  //       print(data);
+  //     } else{
+  //       // print statusCode != 200 data
+  //       debugPrint("Error in getting response");
+  //     }
+  //   } on Exception catch (e) {
+  //     debugPrint(e.toString());
+  //   }
+  // }
 }
