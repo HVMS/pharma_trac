eb0162bd28a1e76b697e54cd9283d422f154b150
HVMS <viralsid2330@gmail.com>
2023-11-01 21:49:34 -0300

    user model updated

--- lib/model/user.model.dart
+++ lib/model/user.model.dart
@@ -1,17 +1,38 @@
-import 'dart:core';
+// import 'dart:core';
+//
+// class Users{
+//   // late String emailAddress;
+//   // // late final String? fullName;
+//   // late String password;
+//   // // late final DateTime? birthDate;
+//   // // late final String? gender;
+//   // // late final String? country;
+//   // // late final double? height;
+//   // // late final double? weight;
+//   //
+//   // // Users({this.emailAddress, this.fullName, this.password, this.birthDate, this.gender, this.country, this.height, this.weight});
+//   //
+//   // Users({required this.emailAddress, required this.password});
+//
+// }
 
-class Users{
-  late final String? emailAddress;
-  // late final String? fullName;
-  late final String? password;
-  // late final DateTime? birthDate;
-  // late final String? gender;
-  // late final String? country;
-  // late final double? height;
-  // late final double? weight;
+class UserRegisterRequestModel {
+  UserRegisterRequestModel({
+    required this.emailAddress,
+    required this.password,
+  });
+  late final String emailAddress;
+  late final String password;
 
-  // Users({this.emailAddress, this.fullName, this.password, this.birthDate, this.gender, this.country, this.height, this.weight});
+  UserRegisterRequestModel.fromJson(Map<String, dynamic> json) {
+    emailAddress = json['email_address'];
+    password = json['password'];
+  }
 
-  Users({this.emailAddress, this.password});
-
+  Map<String, dynamic> toJson() {
+    final data = <String, dynamic>{};
+    data['email_address'] = emailAddress;
+    data['password'] = password;
+    return data;
+  }
 }
