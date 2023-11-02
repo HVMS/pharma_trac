9054adc90d3d958cf519cf7e44a7d95d179a8b6a
HVMS <viralsid2330@gmail.com>
2023-11-02 01:25:15 -0300

    files updated with fake registered api and it worked with validations

--- lib/LoginAndRegistration/login_screen.dart
+++ lib/LoginAndRegistration/login_screen.dart
@@ -8,7 +8,7 @@ import '../Utils/colors_utils.dart';
 import '../model/user_register_response_model.dart';
 import '../services/users_api.dart';
 
-class LoginScreen extends StatefulWidget{
+class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
 
   @override
@@ -16,9 +16,9 @@ class LoginScreen extends StatefulWidget{
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-
   var emailController = TextEditingController();
   var passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -36,170 +36,224 @@ class _LoginScreenState extends State<LoginScreen> {
             ],
           ),
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                CircleAvatar(
-                  radius: 100,
-                  backgroundImage: AssetImage('images/Logo1.png'),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text(
-                          StringUtils.emailAddress,
-                          textAlign: TextAlign.center,
-                          style: GoogleFonts.kiwiMaru(
-                            fontSize: 15.0,
-                            fontWeight: FontWeight.w400,
-                            color: ColorUtils.emailAddressTextColor,
-                          ),
-                        ),
-                        TextFormField(
-                          cursorColor: ColorUtils.black,
-                          decoration: InputDecoration(
-                            fillColor: ColorUtils.white,
-                            filled: true,
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(5.0),
-                              borderSide: BorderSide(
-                                color: ColorUtils.emailAddressTextFieldColor,
-                              ),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                width: 1, // Set the desired width
-                                color: ColorUtils.emailAddressTextFieldColor, // Set the desired color
-                              ),
-                            ),
-                            contentPadding: const EdgeInsets.all(18.0),
-                            hintStyle: TextStyle(color: Colors.grey[600]),
-                            hintText: "Enter your Email Address",
-                          ),
-                          controller: emailController,
-                        ),
-                        const SizedBox(height: 20.0),
-                        Text(
-                          StringUtils.password,
-                          textAlign: TextAlign.center,
-                          style: GoogleFonts.kiwiMaru(
-                            fontSize: 15.0,
-                            fontWeight: FontWeight.w400,
-                            color: ColorUtils.emailAddressTextColor,
-                          ),
-                        ),
-                        TextFormField(
-                          cursorColor: ColorUtils.black,
-                          obscureText: true,
-                          decoration: InputDecoration(
-                            fillColor: ColorUtils.white,
-                            filled: true,
-                            enabledBorder: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(5.0),
-                              borderSide: BorderSide(
-                                color: ColorUtils.emailAddressTextFieldColor,
-                              ),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(
-                                width: 1,
-                                color: ColorUtils.emailAddressTextFieldColor,
-                              ),
-                            ),
-                            contentPadding: const EdgeInsets.all(18.0),
-                            hintStyle: TextStyle(color: Colors.grey[600]),
-                            hintText: "Enter your Password",
-                          ),
-                          controller: passwordController,
-                        ),
-                      ],
+        child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    CircleAvatar(
+                      radius: 100,
+                      backgroundImage: AssetImage('images/Logo1.png'),
                     ),
-                  ),
+                  ],
                 ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    minimumSize: const Size(250, 60),
-                    elevation: 2.0,
-                    backgroundColor: ColorUtils.registerButtonColor,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30.0),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(15.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              StringUtils.emailAddress,
+                              textAlign: TextAlign.center,
+                              style: GoogleFonts.kiwiMaru(
+                                fontSize: 15.0,
+                                fontWeight: FontWeight.w400,
+                                color: ColorUtils.emailAddressTextColor,
+                              ),
+                            ),
+                            TextFormField(
+                                cursorColor: ColorUtils.black,
+                                decoration: InputDecoration(
+                                  fillColor: ColorUtils.white,
+                                  filled: true,
+                                  enabledBorder: OutlineInputBorder(
+                                    borderRadius: BorderRadius.circular(5.0),
+                                    borderSide: BorderSide(
+                                      color:
+                                          ColorUtils.emailAddressTextFieldColor,
+                                    ),
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      width: 1, // Set the desired width
+                                      color: ColorUtils
+                                          .emailAddressTextFieldColor, // Set the desired color
+                                    ),
+                                  ),
+                                  contentPadding: const EdgeInsets.all(18.0),
+                                  hintStyle: TextStyle(color: Colors.grey[600]),
+                                  hintText: "Enter your Email Address",
+                                ),
+                                controller: emailController,
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return 'Please enter some text';
+                                  } else if (!isValidEmail(value)) {
+                                    return 'Invalid email format';
+                                  }
+                                }),
+                            const SizedBox(height: 20.0),
+                            Text(
+                              StringUtils.password,
+                              textAlign: TextAlign.center,
+                              style: GoogleFonts.kiwiMaru(
+                                fontSize: 15.0,
+                                fontWeight: FontWeight.w400,
+                                color: ColorUtils.emailAddressTextColor,
+                              ),
+                            ),
+                            TextFormField(
+                              cursorColor: ColorUtils.black,
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                fillColor: ColorUtils.white,
+                                filled: true,
+                                enabledBorder: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(5.0),
+                                  borderSide: BorderSide(
+                                    color:
+                                        ColorUtils.emailAddressTextFieldColor,
+                                  ),
+                                ),
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    width: 1,
+                                    color:
+                                        ColorUtils.emailAddressTextFieldColor,
+                                  ),
+                                ),
+                                contentPadding: const EdgeInsets.all(18.0),
+                                hintStyle: TextStyle(color: Colors.grey[600]),
+                                hintText: "Enter your Password",
+                              ),
+                              controller: passwordController,
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Password cannot be empty';
+                                } else if (value.length < 6) {
+                                  return 'Password must be at least 6 characters long';
+                                } else if (!isStrongPassword(value)) {
+                                  return 'Password must contain at least one uppercase, one lowercase, one number, and an underscore.';
+                                } else {
+                                  return null;
+                                }
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
-                  ),
-                  onPressed: () {
-                    // var userData = {
-                    //   "email_address" : emailController.text,
-                    //   "password" : passwordController.text,
-                    // };
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        minimumSize: const Size(250, 60),
+                        elevation: 2.0,
+                        backgroundColor: ColorUtils.registerButtonColor,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0),
+                        ),
+                      ),
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          UserRegisterRequestModel userRegisterRequestModel =
+                          UserRegisterRequestModel(
+                            emailAddress: emailController.text,
+                            password: passwordController.text,
+                          );
 
-                    UserRegisterRequestModel userRegisterRequestModel = UserRegisterRequestModel(
-                        emailAddress: emailController.text,
-                        password: passwordController.text
-                    );
-
-                    UsersAPI.register(userRegisterRequestModel).then((value) => {
-                      print(value),
-                      print(value.runtimeType),
-                    });
-
-                  },
-                  child: Text(
-                    StringUtils.signUp,
-                    style: GoogleFonts.kiwiMaru(
-                      fontSize: 25.0,
-                      fontWeight: FontWeight.w600,
-                      color: ColorUtils.emailAddressTextColor,
+                          UsersAPI.register(userRegisterRequestModel)
+                              .then((response) {
+                            print(response);
+                            print(response.runtimeType);
+                            if (response is String) {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(
+                                  content: Text("Registration failed: $response"),
+                                ),
+                              );
+                            } else {
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                  content: Text("Registration successful"),
+                                ),
+                              );
+                            }
+                          });
+                        }
+                      },
+                      child: Text(
+                        StringUtils.signUp,
+                        style: GoogleFonts.kiwiMaru(
+                          fontSize: 25.0,
+                          fontWeight: FontWeight.w600,
+                          color: ColorUtils.emailAddressTextColor,
+                        ),
+                      ),
                     ),
-                  ),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      StringUtils.registerDirectionText,
+                      style: GoogleFonts.kiwiMaru(
+                        fontSize: 15.0,
+                        fontWeight: FontWeight.w600,
+                        color: ColorUtils.emailAddressTextColor,
+                      ),
+                    ),
+                    const SizedBox(width: 10),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.of(context).push(MaterialPageRoute(
+                            builder: (context) => const RegisterScreen()));
+                      },
+                      child: Text(
+                        StringUtils.registerHereText,
+                        style: GoogleFonts.kiwiMaru(
+                          decoration: TextDecoration.underline,
+                          decorationThickness: 2,
+                          fontSize: 15.0,
+                          fontWeight: FontWeight.w600,
+                          color: ColorUtils.splashScreenTextColor,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  StringUtils.registerDirectionText,
-                  style: GoogleFonts.kiwiMaru(
-                    fontSize: 15.0,
-                    fontWeight: FontWeight.w600,
-                    color: ColorUtils.emailAddressTextColor,
-                  ),
-                ),
-                const SizedBox(width: 10),
-                GestureDetector(
-                  onTap: () { Navigator.of(context).push(MaterialPageRoute( builder: (context) => const RegisterScreen())); } ,
-                  child: Text(
-                    StringUtils.registerHereText,
-                    style: GoogleFonts.kiwiMaru(
-                      decoration: TextDecoration.underline,
-                      decorationThickness: 2,
-                      fontSize: 15.0,
-                      fontWeight: FontWeight.w600,
-                      color: ColorUtils.splashScreenTextColor,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
+            )),
       ),
     );
   }
 
+  bool isValidEmail(String email) {
+    final emailRegExp = RegExp(
+      r'^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$',
+    );
+    return emailRegExp.hasMatch(email) && email.contains('@');
+  }
+
+  bool isStrongPassword(String password) {
+    // Check if the password contains at least one uppercase letter, one lowercase letter,
+    // one number, and an underscore.
+    final hasUppercase = RegExp(r'[A-Z]').hasMatch(password);
+    final hasLowercase = RegExp(r'[a-z]').hasMatch(password);
+    final hasNumber = RegExp(r'[0-9]').hasMatch(password);
+    final hasUnderscore = password.contains('_');
+
+    return hasUppercase && hasLowercase && hasNumber && hasUnderscore;
+  }
 }
--- lib/services/users_api.dart
+++ lib/services/users_api.dart
@@ -11,49 +11,35 @@ class UsersAPI{
 
   static var client = http.Client();
 
-  static Future<String> register(UserRegisterRequestModel model) async {
+  static Future<dynamic> register(UserRegisterRequestModel model) async {
     Map<String, String> requestHeaders = {
       'content-type' : 'application/json',
     };
 
     var urlFinal = Uri.parse("${baseURL}register");
 
-    var response = await client.post(
-      urlFinal,
-      headers: requestHeaders,
-      body: jsonEncode(model.toJson()),
-    );
+    try {
+      var response = await client.post(
+        urlFinal,
+        headers: requestHeaders,
+        body: jsonEncode(model.toJson()),
+      );
 
-    if(response.statusCode == 200){
-      //
-      return response.body;
-    }else{
-      //
-      return response.body;
+      Map<String, dynamic> jsonResponse = json.decode(response.body);
+      UserRegisterResponseModel userRegisterResponseModel = UserRegisterResponseModel.fromJson(jsonResponse);
+
+      if(userRegisterResponseModel.statusCode == 200){
+
+        return userRegisterResponseModel.user;
+
+      }else{
+        return userRegisterResponseModel.message;
+        // return 'Error: ${response.statusCode} - ${json.decode(response.body)['message']}';
+      }
+    } on Exception catch (e) {
+      // TODO
+      debugPrint(e.toString());
+      rethrow;
     }
   }
-
-  // static addUser(Map userData) async{
-  //
-  //   print(userData);
-  //
-  //   var addUserURL = Uri.parse("${baseURL}register");
-  //
-  //   print("Hello wrold");
-  //
-  //   try {
-  //     final res = await http.post(addUserURL, body: userData);
-  //
-  //     if (res.statusCode == 200){
-  //       // data
-  //       var data = jsonDecode(res.body.toString());
-  //       print(data);
-  //     } else{
-  //       // print statusCode != 200 data
-  //       debugPrint("Error in getting response");
-  //     }
-  //   } on Exception catch (e) {
-  //     debugPrint(e.toString());
-  //   }
-  // }
 }
