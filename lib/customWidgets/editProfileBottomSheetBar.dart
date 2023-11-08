1ba55487daa2be9308e6b3baf5cbc555a18a0ebd
HVMS <viralsid2330@gmail.com>
2023-11-07 23:36:39 -0400

    edit custom bottom sheet bar completed

--- lib/customWidgets/editProfileBottomSheetBar.dart
+++ lib/customWidgets/editProfileBottomSheetBar.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../Utils/colors_utils.dart';
+import '../Utils/string_utils.dart';
+import 'CustomGreyDivider.dart';
 
 class EditProfileBottomSheetBar extends StatefulWidget{
-  final Function(String) onTextSubmitted;
 
-  const EditProfileBottomSheetBar({super.key, required this.onTextSubmitted});
+  const EditProfileBottomSheetBar({super.key});
 
   @override
   State<StatefulWidget> createState() => _EditProfileBottomSheetBar();
@@ -17,29 +22,91 @@ class _EditProfileBottomSheetBar extends State<EditProfileBottomSheetBar>{
   @override
   Widget build(BuildContext context) {
 
-    return Container(
-      padding: const EdgeInsets.all(16.0),
-      color: Colors.white,
+    return SizedBox(
+      height: 300.0,
       child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          const Text('Custom BottomSheet Bar'),
-          TextField(
-            controller: _textController,
-            decoration: const InputDecoration(
-              hintText: 'Enter your text',
-            ),
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  IconButton(
+                    icon: SvgPicture.asset('Icons/close_button_icon.svg'),
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  Expanded(
+                    child: Center(
+                      child: Text('Update your Email Address',
+                        style: GoogleFonts.kiwiMaru(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w700,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              const CustomGreyDivider(),
+            ],
           ),
-          ElevatedButton(
-            onPressed: () {
-              widget.onTextSubmitted(_textController.text);
-              Navigator.pop(context);
-            },
-            child: const Text('Submit'),
+          Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: _textController,
+                  decoration: const InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white,
+                    hintText: 'Enter your text',
+                    contentPadding: EdgeInsets.all(10.0),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black, width: 2.0),
+                    ),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.black, width: 2.0),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          Column(
+            children: [
+              const CustomGreyDivider(),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: const Size(250, 60),
+                    elevation: 2.0,
+                    backgroundColor: ColorUtils.registerButtonColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30.0),
+                    ),
+                  ),
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text(
+                    StringUtils.update,
+                    style: GoogleFonts.kiwiMaru(
+                      fontSize: 25.0,
+                      fontWeight: FontWeight.w600,
+                      color: ColorUtils.emailAddressTextColor,
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ],
       ),
     );
-
   }
 }
