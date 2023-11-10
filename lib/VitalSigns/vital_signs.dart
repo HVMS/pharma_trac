5d868898bd2cd810b87c1de54b33d1f8899a969a
HVMS <viralsid2330@gmail.com>
2023-11-10 04:38:10 -0400

    vital signs updated

--- lib/VitalSigns/vital_signs.dart
+++ lib/VitalSigns/vital_signs.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hive/hive.dart';
+import 'package:pharma_trac/VitalSigns/blood_cholesterol.dart';
+import 'package:pharma_trac/VitalSigns/blood_pessure_sign.dart';
+import 'package:pharma_trac/VitalSigns/blood_sugar_sign.dart';
+import 'package:pharma_trac/VitalSigns/body_temperature.dart';
 
 import '../Utils/colors_utils.dart';
 import '../Utils/timeUtils.dart';
@@ -31,7 +36,7 @@ class _VitalSignsState extends State<VitalSigns> {
         title: Text(
           TimeUtils.greetingMessage() + userDataBox.get("fullName", defaultValue: ''),
           style: GoogleFonts.roboto(
-            fontSize: 25.0,
+            fontSize: 18.0,
             fontWeight: FontWeight.w700,
             color: ColorUtils.white,
           ),
@@ -45,6 +50,28 @@ class _VitalSignsState extends State<VitalSigns> {
           padding: const EdgeInsets.all(10.0),
           child: Card(
             color: ColorUtils.white,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+            child: ListView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              itemCount: 1, // A single child in the ListView.builder
+              itemBuilder: (context, index) {
+                return GridView.count(
+                  crossAxisCount: 2, // Two cards in each row
+                  padding: const EdgeInsets.all(10.0),
+                  childAspectRatio: 1.2, // Adjust as needed for your design
+                  shrinkWrap: true,
+                  children: const <Widget>[
+                    BloodPressureSign(),
+                    BloodSugarSign(),
+                    BodyTemperature(),
+                    BloodCholesterol(),
+                  ],
+                );
+              },
+            ),
           ),
         ),
       ),
